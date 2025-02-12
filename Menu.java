48663478348e61ae227164b7c339ccac568b05cc
Beatriz Corrêa <bcorreacontato@gmail.com>
2025-02-11 21:05:21 -0300

    main

---
+++ Menu.java
@@ -0,0 +1,42 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// executando as classes
+		
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		
+		while (true) {
+		
+		System.out.println("******************************************");
+		System.out.println("                                          ");
+		System.out.println("           BANCO DO BRAZIL COM Z          ");
+		System.out.println("                                          ");
+		System.out.println("******************************************");
+		System.out.println("                                          ");
+		System.out.println("     1 - Criar conta                      ");
+		System.out.println("     2 - Listar todas as contas           ");
+		System.out.println("     3 - Buscar conta por número          ");
+		System.out.println("     4 - Atualizar dados da conta         ");
+	    System.out.println("     5 - Apagar conta                     ");
+	    System.out.println("     6 - Sacar                            ");
+	    System.out.println("     7 - Depositar                        ");
+	    System.out.println("     8 - Transferir valores entre contas  ");
+	    System.out.println("     9 - Sair                             ");
+	    System.out.println("                                          ");
+	    System.out.println("******************************************");
+	    System.out.println("Entre com a opção desejada:               ");
+	    System.out.println("                                          ");
+	    
+	    opcao = leia.nextInt();
+	    
+	    
+	    		
+	
+		}
+	}
+}
---
+++ until/cores.java
@@ -0,0 +1,13 @@
+package conta.until;
+
+public class cores {
+	
+	//reset color
+	public static final String TEXT_RESET = "\u001B[0m";
+	
+	//text color 
+	public static final String TEXT_GREEN = "\\033[0;95m";
+	
+	}
+
+
